93af6a3ddbbde3999db36200af85ad3d21d376d1
devYeonjun <alcls0026@gmail.com>
2022-04-25 11:13:25 +1200

    Add user profile & alerts

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -9,30 +9,56 @@ const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
 export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
+    user: {},
+    repos: [],
     loading: false,
   }
 
   const [state, dispatch] = useReducer(githubReducer, initialState)
 
-  //   Get search results
-  const searchUsers = async (text) => {
+  
+  //   Get single user
+  const getUser = async (login) => {
     setLoading()
 
-    const params = new URLSearchParams({
-      q: text,
-    })
-
-    const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
+    const response = await fetch(`${GITHUB_URL}/users/${login}`, {
       headers: {
         Authorization: `token ${GITHUB_TOKEN}`,
       },
     })
 
-    const { items } = await response.json()
+    if (response.status === 404) {
+      window.location = '/notfound'
+    } else {
+      const data = await response.json()
+
+      dispatch({
+        type: 'GET_USER',
+        payload: data,
+      })
+    }
+  }
+
+  //   Get user repos
+  const getUserRepos = async (login) => {
+    setLoading()
+
+    const params = new URLSearchParams({
+      sort: 'created',
+      per_page: 10
+    })
+
+    const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`, {
+      headers: {
+        Authorization: `token ${GITHUB_TOKEN}`,
+      },
+    })
+
+    const { data } = await response.json()
 
     dispatch({
-      type: 'GET_USERS',
-      payload: items,
+      type: 'GET_REPOS',
+      payload: data,
     })
   }
 
@@ -45,10 +71,11 @@ export const GithubProvider = ({ children }) => {
   return (
     <GithubContext.Provider
       value={{
-        users: state.users,
-        loading: state.loading,
-        searchUsers,
+        ...state,
+        dispatch,
         clearUsers,
+        getUser,
+        getUserRepos,
       }}
     >
       {children}
